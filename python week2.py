7ca251f8ba1794ba57e2476a51708f4fae117594
Boda Rajesh <154888255+RajeshBodatech@users.noreply.github.com>
2024-04-04 17:38:34 +0530

    Add files via upload

---
+++ python week2.py
@@ -0,0 +1,326 @@
+
+  cat> grepfile.txt
+ 1950  grep -c "unix" grepfile.txt
+ 1951  cat> grepfile.txt
+ 1952  grep -h "commands" grepfile.txt
+ 1953  grep -l "commands" grepfile.txt
+ 1954  grep -n "commands" grepfile.txt
+ 1955  grep -v "commands" grepfile.txt
+ 1956  grep -e "commands" grepfile.txt
+ 1957  grep -o "commands" grepfile.txt
+ 1958  cat > names.txt
+ 1959  sort names.txt
+ 1960  sort -n names.txt
+ 1961  cat > numbers.txt
+ 1962  sort -n numbers.txt
+ 1963  sort -u numbers.txt
+ 1964  cat > sedfile.txt
+ 1965  sed 's/command/COMMAND/' sedfile.txt
+ 1966  sed '/COMMAND/d' sedfile.txt
+ 1967  sed '/command/d' sedfile.txt
+ 1968  cat grepfile.txt
+ 1969  sed '/one/d' grepfile.txt
+ 1970  sed 's/unix/UNIX/' grepfile.txt
+ 1971  cat> awkfile.txt
+ 1972  $ awk '{print $1,$2}' awkfile.txt
+ 1973  $ awk ' {print $1,$2} ' awkfile.txt
+ 1974  $ awk ' {print $1,$2,$3,$4} ' awkfile.txt
+ 1975  $ awk ' {print $3,$4} ' awkfile.txt
+ 1976  ELELELLELELELLE[LE{; ;; ;
+ 1977  MAWK
+ 1978  MAWK AWK
+ 1979  MAN AWK
+ 1980  shutdown -r
+ 1981  -c
+ 1982  shudown -c
+ 1983  shutdown -c
+ 1984  'shutdown -c'
+ 1985  shutdown -r+2
+ 1986  shutdown -r+5
+ 1987  shutdown -r +5
+ 1988  'shutdown -c'
+ 1989  shutdown -c
+ 1990  shutdown -r +1
+ 1991  histort
+ 1992  history
+(base) rgukt-basar@rajesh-the-learner:~$ chmod  u+x numbers.txt
+(base) rgukt-basar@rajesh-the-learner:~$ la -ltr
+total 976
+-rwxr-----  1 rgukt-basar rgukt-basar  23157 Jan 28  2021  mypic.jpg
+drwx------  3 rgukt-basar rgukt-basar   4096 Jan  5  2023  .pki
+-rw-r--r--  1 rgukt-basar rgukt-basar    807 Jan  5  2023  .profile
+-rw-r--r--  1 rgukt-basar rgukt-basar    220 Jan  5  2023  .bash_logout
+drwx------  3 rgukt-basar rgukt-basar   4096 Jan  5  2023  .local
+drwxr-xr-x  2 rgukt-basar rgukt-basar   4096 Jan  5  2023  Public
+drwx------  2 rgukt-basar rgukt-basar   4096 Jan  5  2023  .ssh
+-rw-r--r--  1 rgukt-basar rgukt-basar      0 Jan  5  2023  .sudo_as_admin_successful
+drwx------  3 rgukt-basar rgukt-basar   4096 Jun 26  2023  .gnome
+-rwxr-----  1 rgukt-basar rgukt-basar 571521 Jun 27  2023  RK.jpg
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Jul  9  2023  .gphoto
+-rw-------  1 rgukt-basar rgukt-basar     40 Jul 15  2023  .python_history
+drwx------  6 rgukt-basar rgukt-basar   4096 Jul 15  2023  .thunderbird
+drwx------  3 rgukt-basar rgukt-basar   4096 Jul 15  2023  .mozilla
+drwxrwxr-x  4 rgukt-basar rgukt-basar   4096 Aug  6  2023  .vscode
+drwxrwxr-x  3 rgukt-basar rgukt-basar   4096 Aug  6  2023  .dotnet
+drwxrwxrwx  7 rgukt-basar rgukt-basar   4096 Aug 11  2023  c
+drwxr-xr-x  2 rgukt-basar rgukt-basar   4096 Aug 22  2023  ass2
+drwxr-xr-x  2 rgukt-basar rgukt-basar   4096 Sep 17  2023  Templates
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Sep 25  2023  hash
+drwxr-xr-x  2 rgukt-basar rgukt-basar   4096 Sep 26  2023  vin
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Sep 26  2023  stack
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Sep 26  2023  graph
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Oct  5 22:42  dsa
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Oct 11 17:14  tree
+-rw-rw-r--  1 rgukt-basar rgukt-basar    209 Nov  6 15:46  .wget-hsts
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Nov 13 22:53  .redhat
+drwxrwxr-x  4 rgukt-basar rgukt-basar   4096 Nov 13 23:01  .java
+drwx------  2 rgukt-basar rgukt-basar   4096 Nov 19 18:09  .dosbox
+-rw-rw-r--  1 rgukt-basar rgukt-basar   3394 Feb  1 13:49  B200101_labinternal.sql
+-rw-rw-r--  1 rgukt-basar rgukt-basar   3394 Feb  1 15:58  labinternal.sql
+drwxrwxr-x  6 rgukt-basar rgukt-basar   4096 Feb  6 00:04  unknown
+-rw-rw-r--  1 rgukt-basar rgukt-basar    306 Feb  6 15:20  p2.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1566 Feb 10 13:47 'MovieTheatre$Person.class'
+-rw-rw-r--  1 rgukt-basar rgukt-basar    880 Feb 10 13:47  MovieTheatre.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1352 Feb 10 13:54 'movie$Person.class'
+-rw-rw-r--  1 rgukt-basar rgukt-basar   2137 Feb 10 22:14 'Untitled Document 1'
+-rw-rw-r--  1 rgukt-basar rgukt-basar   6349 Feb 11 10:32  dec.sql
+-rw-rw-r--  1 rgukt-basar rgukt-basar   8580 Feb 14 14:08  p4.java
+-rw-rw-r--  1 rgukt-basar rgukt-basar    949 Feb 22 10:14  sbuttonexample.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar    290 Feb 22 10:14  java.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar    967 Feb 22 10:24  java.java
+-rw-rw-r--  1 rgukt-basar rgukt-basar    181 Mar  3 15:56  A.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar    569 Mar  3 16:20  p3.java
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1089 Mar  3 16:20  student.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar    422 Mar  3 16:20  p3.class
+drwxrwxr-x  3 rgukt-basar rgukt-basar   4096 Mar 11 11:14  .m2
+drwxrwxr-x  4 rgukt-basar rgukt-basar   4096 Mar 11 11:24  NetBeansProjects
+drwxrwxr-x  3 rgukt-basar rgukt-basar   4096 Mar 13 17:11  html2
+drwx------ 10 rgukt-basar rgukt-basar   4096 Mar 13 20:26  snap
+drwxrwxr-x 14 rgukt-basar rgukt-basar   4096 Mar 14 16:05  JAVA
+drwx------ 46 rgukt-basar rgukt-basar   4096 Mar 15 17:12  .cache
+drwxr-xr-x  3 rgukt-basar rgukt-basar   4096 Mar 18 14:41  Music
+drwxrwxr-x  4 rgukt-basar rgukt-basar   4096 Mar 20 12:04  Desktop
+drwxrwxr-x  3 rgukt-basar rgukt-basar   4096 Mar 20 14:02  python.py
+drwxr-xr-x  5 rgukt-basar rgukt-basar   4096 Mar 20 14:11  Pictures
+-rw-rw-r--  1 rgukt-basar rgukt-basar    269 Mar 20 14:56  fcp.c
+drwx------  9 rgukt-basar rgukt-basar   4096 Mar 20 21:46  Documents
+drwx------ 29 rgukt-basar rgukt-basar   4096 Mar 21 16:50  .config
+-rw-rw-r--  1 rgukt-basar rgukt-basar    683 Mar 24 13:03  p5.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1787 Mar 24 21:06  stringmatch.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar    863 Mar 25 23:17  stringmatch1.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar    815 Mar 25 23:59  raj.c
+drwxrwxr-x  5 rgukt-basar rgukt-basar   4096 Mar 26 00:12  Videos
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Mar 26 20:48  .conda
+drwxrwxr-x 31 rgukt-basar rgukt-basar   4096 Mar 26 20:48  anaconda3
+-rw-r--r--  1 rgukt-basar rgukt-basar   4270 Mar 26 20:48  .bashrc
+-rw-rw-r--  1 rgukt-basar rgukt-basar    487 Mar 27 14:38  stringmatch2.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar    893 Mar 27 15:36  closestpair.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1523 Mar 27 15:54 'Untitled Document 2'
+drwx------  2 rgukt-basar rgukt-basar   4096 Mar 27 16:02  .gnupg
+-rwxrwxr-x  1 rgukt-basar rgukt-basar  16152 Mar 27 16:31  a.out
+-rw-rw-r--  1 rgukt-basar rgukt-basar    738 Mar 27 16:35  closestpair1.c
+drwx------ 17 rgukt-basar rgukt-basar  28672 Mar 28 14:34  Downloads
+-rw-rw-r--  1 rgukt-basar rgukt-basar     95 Mar 28 14:40  grepfile.txt
+-rw-rw-r--  1 rgukt-basar rgukt-basar     65 Mar 28 14:45  names.txt
+-rwxrw-r--  1 rgukt-basar rgukt-basar     22 Mar 28 14:47  numbers.txt
+-rw-rw-r--  1 rgukt-basar rgukt-basar    485 Mar 28 14:52  sedfile.txt
+-rw-rw-r--  1 rgukt-basar rgukt-basar     63 Mar 28 15:06  awkfile.txt
+-rw-------  1 rgukt-basar rgukt-basar  29577 Mar 28 15:21  .bash_history
+(base) rgukt-basar@rajesh-the-learner:~$ la -lrt
+total 976
+-rwxr-----  1 rgukt-basar rgukt-basar  23157 Jan 28  2021  mypic.jpg
+drwx------  3 rgukt-basar rgukt-basar   4096 Jan  5  2023  .pki
+-rw-r--r--  1 rgukt-basar rgukt-basar    807 Jan  5  2023  .profile
+-rw-r--r--  1 rgukt-basar rgukt-basar    220 Jan  5  2023  .bash_logout
+drwx------  3 rgukt-basar rgukt-basar   4096 Jan  5  2023  .local
+drwxr-xr-x  2 rgukt-basar rgukt-basar   4096 Jan  5  2023  Public
+drwx------  2 rgukt-basar rgukt-basar   4096 Jan  5  2023  .ssh
+-rw-r--r--  1 rgukt-basar rgukt-basar      0 Jan  5  2023  .sudo_as_admin_successful
+drwx------  3 rgukt-basar rgukt-basar   4096 Jun 26  2023  .gnome
+-rwxr-----  1 rgukt-basar rgukt-basar 571521 Jun 27  2023  RK.jpg
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Jul  9  2023  .gphoto
+-rw-------  1 rgukt-basar rgukt-basar     40 Jul 15  2023  .python_history
+drwx------  6 rgukt-basar rgukt-basar   4096 Jul 15  2023  .thunderbird
+drwx------  3 rgukt-basar rgukt-basar   4096 Jul 15  2023  .mozilla
+drwxrwxr-x  4 rgukt-basar rgukt-basar   4096 Aug  6  2023  .vscode
+drwxrwxr-x  3 rgukt-basar rgukt-basar   4096 Aug  6  2023  .dotnet
+drwxrwxrwx  7 rgukt-basar rgukt-basar   4096 Aug 11  2023  c
+drwxr-xr-x  2 rgukt-basar rgukt-basar   4096 Aug 22  2023  ass2
+drwxr-xr-x  2 rgukt-basar rgukt-basar   4096 Sep 17  2023  Templates
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Sep 25  2023  hash
+drwxr-xr-x  2 rgukt-basar rgukt-basar   4096 Sep 26  2023  vin
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Sep 26  2023  stack
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Sep 26  2023  graph
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Oct  5 22:42  dsa
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Oct 11 17:14  tree
+-rw-rw-r--  1 rgukt-basar rgukt-basar    209 Nov  6 15:46  .wget-hsts
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Nov 13 22:53  .redhat
+drwxrwxr-x  4 rgukt-basar rgukt-basar   4096 Nov 13 23:01  .java
+drwx------  2 rgukt-basar rgukt-basar   4096 Nov 19 18:09  .dosbox
+-rw-rw-r--  1 rgukt-basar rgukt-basar   3394 Feb  1 13:49  B200101_labinternal.sql
+-rw-rw-r--  1 rgukt-basar rgukt-basar   3394 Feb  1 15:58  labinternal.sql
+drwxrwxr-x  6 rgukt-basar rgukt-basar   4096 Feb  6 00:04  unknown
+-rw-rw-r--  1 rgukt-basar rgukt-basar    306 Feb  6 15:20  p2.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1566 Feb 10 13:47 'MovieTheatre$Person.class'
+-rw-rw-r--  1 rgukt-basar rgukt-basar    880 Feb 10 13:47  MovieTheatre.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1352 Feb 10 13:54 'movie$Person.class'
+-rw-rw-r--  1 rgukt-basar rgukt-basar   2137 Feb 10 22:14 'Untitled Document 1'
+-rw-rw-r--  1 rgukt-basar rgukt-basar   6349 Feb 11 10:32  dec.sql
+-rw-rw-r--  1 rgukt-basar rgukt-basar   8580 Feb 14 14:08  p4.java
+-rw-rw-r--  1 rgukt-basar rgukt-basar    949 Feb 22 10:14  sbuttonexample.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar    290 Feb 22 10:14  java.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar    967 Feb 22 10:24  java.java
+-rw-rw-r--  1 rgukt-basar rgukt-basar    181 Mar  3 15:56  A.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar    569 Mar  3 16:20  p3.java
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1089 Mar  3 16:20  student.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar    422 Mar  3 16:20  p3.class
+drwxrwxr-x  3 rgukt-basar rgukt-basar   4096 Mar 11 11:14  .m2
+drwxrwxr-x  4 rgukt-basar rgukt-basar   4096 Mar 11 11:24  NetBeansProjects
+drwxrwxr-x  3 rgukt-basar rgukt-basar   4096 Mar 13 17:11  html2
+drwx------ 10 rgukt-basar rgukt-basar   4096 Mar 13 20:26  snap
+drwxrwxr-x 14 rgukt-basar rgukt-basar   4096 Mar 14 16:05  JAVA
+drwx------ 46 rgukt-basar rgukt-basar   4096 Mar 15 17:12  .cache
+drwxr-xr-x  3 rgukt-basar rgukt-basar   4096 Mar 18 14:41  Music
+drwxrwxr-x  4 rgukt-basar rgukt-basar   4096 Mar 20 12:04  Desktop
+drwxrwxr-x  3 rgukt-basar rgukt-basar   4096 Mar 20 14:02  python.py
+drwxr-xr-x  5 rgukt-basar rgukt-basar   4096 Mar 20 14:11  Pictures
+-rw-rw-r--  1 rgukt-basar rgukt-basar    269 Mar 20 14:56  fcp.c
+drwx------  9 rgukt-basar rgukt-basar   4096 Mar 20 21:46  Documents
+drwx------ 29 rgukt-basar rgukt-basar   4096 Mar 21 16:50  .config
+-rw-rw-r--  1 rgukt-basar rgukt-basar    683 Mar 24 13:03  p5.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1787 Mar 24 21:06  stringmatch.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar    863 Mar 25 23:17  stringmatch1.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar    815 Mar 25 23:59  raj.c
+drwxrwxr-x  5 rgukt-basar rgukt-basar   4096 Mar 26 00:12  Videos
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Mar 26 20:48  .conda
+drwxrwxr-x 31 rgukt-basar rgukt-basar   4096 Mar 26 20:48  anaconda3
+-rw-r--r--  1 rgukt-basar rgukt-basar   4270 Mar 26 20:48  .bashrc
+-rw-rw-r--  1 rgukt-basar rgukt-basar    487 Mar 27 14:38  stringmatch2.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar    893 Mar 27 15:36  closestpair.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1523 Mar 27 15:54 'Untitled Document 2'
+drwx------  2 rgukt-basar rgukt-basar   4096 Mar 27 16:02  .gnupg
+-rwxrwxr-x  1 rgukt-basar rgukt-basar  16152 Mar 27 16:31  a.out
+-rw-rw-r--  1 rgukt-basar rgukt-basar    738 Mar 27 16:35  closestpair1.c
+drwx------ 17 rgukt-basar rgukt-basar  28672 Mar 28 14:34  Downloads
+-rw-rw-r--  1 rgukt-basar rgukt-basar     95 Mar 28 14:40  grepfile.txt
+-rw-rw-r--  1 rgukt-basar rgukt-basar     65 Mar 28 14:45  names.txt
+-rwxrw-r--  1 rgukt-basar rgukt-basar     22 Mar 28 14:47  numbers.txt
+-rw-rw-r--  1 rgukt-basar rgukt-basar    485 Mar 28 14:52  sedfile.txt
+-rw-rw-r--  1 rgukt-basar rgukt-basar     63 Mar 28 15:06  awkfile.txt
+-rw-------  1 rgukt-basar rgukt-basar  29577 Mar 28 15:21  .bash_history
+(base) rgukt-basar@rajesh-the-learner:~$ chmod  u-x numbers.txt
+(base) rgukt-basar@rajesh-the-learner:~$ la -lrt
+total 976
+-rwxr-----  1 rgukt-basar rgukt-basar  23157 Jan 28  2021  mypic.jpg
+drwx------  3 rgukt-basar rgukt-basar   4096 Jan  5  2023  .pki
+-rw-r--r--  1 rgukt-basar rgukt-basar    807 Jan  5  2023  .profile
+-rw-r--r--  1 rgukt-basar rgukt-basar    220 Jan  5  2023  .bash_logout
+drwx------  3 rgukt-basar rgukt-basar   4096 Jan  5  2023  .local
+drwxr-xr-x  2 rgukt-basar rgukt-basar   4096 Jan  5  2023  Public
+drwx------  2 rgukt-basar rgukt-basar   4096 Jan  5  2023  .ssh
+-rw-r--r--  1 rgukt-basar rgukt-basar      0 Jan  5  2023  .sudo_as_admin_successful
+drwx------  3 rgukt-basar rgukt-basar   4096 Jun 26  2023  .gnome
+-rwxr-----  1 rgukt-basar rgukt-basar 571521 Jun 27  2023  RK.jpg
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Jul  9  2023  .gphoto
+-rw-------  1 rgukt-basar rgukt-basar     40 Jul 15  2023  .python_history
+drwx------  6 rgukt-basar rgukt-basar   4096 Jul 15  2023  .thunderbird
+drwx------  3 rgukt-basar rgukt-basar   4096 Jul 15  2023  .mozilla
+drwxrwxr-x  4 rgukt-basar rgukt-basar   4096 Aug  6  2023  .vscode
+drwxrwxr-x  3 rgukt-basar rgukt-basar   4096 Aug  6  2023  .dotnet
+drwxrwxrwx  7 rgukt-basar rgukt-basar   4096 Aug 11  2023  c
+drwxr-xr-x  2 rgukt-basar rgukt-basar   4096 Aug 22  2023  ass2
+drwxr-xr-x  2 rgukt-basar rgukt-basar   4096 Sep 17  2023  Templates
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Sep 25  2023  hash
+drwxr-xr-x  2 rgukt-basar rgukt-basar   4096 Sep 26  2023  vin
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Sep 26  2023  stack
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Sep 26  2023  graph
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Oct  5 22:42  dsa
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Oct 11 17:14  tree
+-rw-rw-r--  1 rgukt-basar rgukt-basar    209 Nov  6 15:46  .wget-hsts
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Nov 13 22:53  .redhat
+drwxrwxr-x  4 rgukt-basar rgukt-basar   4096 Nov 13 23:01  .java
+drwx------  2 rgukt-basar rgukt-basar   4096 Nov 19 18:09  .dosbox
+-rw-rw-r--  1 rgukt-basar rgukt-basar   3394 Feb  1 13:49  B200101_labinternal.sql
+-rw-rw-r--  1 rgukt-basar rgukt-basar   3394 Feb  1 15:58  labinternal.sql
+drwxrwxr-x  6 rgukt-basar rgukt-basar   4096 Feb  6 00:04  unknown
+-rw-rw-r--  1 rgukt-basar rgukt-basar    306 Feb  6 15:20  p2.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1566 Feb 10 13:47 'MovieTheatre$Person.class'
+-rw-rw-r--  1 rgukt-basar rgukt-basar    880 Feb 10 13:47  MovieTheatre.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1352 Feb 10 13:54 'movie$Person.class'
+-rw-rw-r--  1 rgukt-basar rgukt-basar   2137 Feb 10 22:14 'Untitled Document 1'
+-rw-rw-r--  1 rgukt-basar rgukt-basar   6349 Feb 11 10:32  dec.sql
+-rw-rw-r--  1 rgukt-basar rgukt-basar   8580 Feb 14 14:08  p4.java
+-rw-rw-r--  1 rgukt-basar rgukt-basar    949 Feb 22 10:14  sbuttonexample.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar    290 Feb 22 10:14  java.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar    967 Feb 22 10:24  java.java
+-rw-rw-r--  1 rgukt-basar rgukt-basar    181 Mar  3 15:56  A.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar    569 Mar  3 16:20  p3.java
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1089 Mar  3 16:20  student.class
+-rw-rw-r--  1 rgukt-basar rgukt-basar    422 Mar  3 16:20  p3.class
+drwxrwxr-x  3 rgukt-basar rgukt-basar   4096 Mar 11 11:14  .m2
+drwxrwxr-x  4 rgukt-basar rgukt-basar   4096 Mar 11 11:24  NetBeansProjects
+drwxrwxr-x  3 rgukt-basar rgukt-basar   4096 Mar 13 17:11  html2
+drwx------ 10 rgukt-basar rgukt-basar   4096 Mar 13 20:26  snap
+drwxrwxr-x 14 rgukt-basar rgukt-basar   4096 Mar 14 16:05  JAVA
+drwx------ 46 rgukt-basar rgukt-basar   4096 Mar 15 17:12  .cache
+drwxr-xr-x  3 rgukt-basar rgukt-basar   4096 Mar 18 14:41  Music
+drwxrwxr-x  4 rgukt-basar rgukt-basar   4096 Mar 20 12:04  Desktop
+drwxrwxr-x  3 rgukt-basar rgukt-basar   4096 Mar 20 14:02  python.py
+drwxr-xr-x  5 rgukt-basar rgukt-basar   4096 Mar 20 14:11  Pictures
+-rw-rw-r--  1 rgukt-basar rgukt-basar    269 Mar 20 14:56  fcp.c
+drwx------  9 rgukt-basar rgukt-basar   4096 Mar 20 21:46  Documents
+drwx------ 29 rgukt-basar rgukt-basar   4096 Mar 21 16:50  .config
+-rw-rw-r--  1 rgukt-basar rgukt-basar    683 Mar 24 13:03  p5.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1787 Mar 24 21:06  stringmatch.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar    863 Mar 25 23:17  stringmatch1.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar    815 Mar 25 23:59  raj.c
+drwxrwxr-x  5 rgukt-basar rgukt-basar   4096 Mar 26 00:12  Videos
+drwxrwxr-x  2 rgukt-basar rgukt-basar   4096 Mar 26 20:48  .conda
+drwxrwxr-x 31 rgukt-basar rgukt-basar   4096 Mar 26 20:48  anaconda3
+-rw-r--r--  1 rgukt-basar rgukt-basar   4270 Mar 26 20:48  .bashrc
+-rw-rw-r--  1 rgukt-basar rgukt-basar    487 Mar 27 14:38  stringmatch2.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar    893 Mar 27 15:36  closestpair.c
+-rw-rw-r--  1 rgukt-basar rgukt-basar   1523 Mar 27 15:54 'Untitled Document 2'
+drwx------  2 rgukt-basar rgukt-basar   4096 Mar 27 16:02  .gnupg
+-rwxrwxr-x  1 rgukt-basar rgukt-basar  16152 Mar 27 16:31  a.out
+-rw-rw-r--  1 rgukt-basar rgukt-basar    738 Mar 27 16:35  closestpair1.c
+drwx------ 17 rgukt-basar rgukt-basar  28672 Mar 28 14:34  Downloads
+-rw-rw-r--  1 rgukt-basar rgukt-basar     95 Mar 28 14:40  grepfile.txt
+-rw-rw-r--  1 rgukt-basar rgukt-basar     65 Mar 28 14:45  names.txt
+-rw-rw-r--  1 rgukt-basar rgukt-basar     22 Mar 28 14:47  numbers.txt
+-rw-rw-r--  1 rgukt-basar rgukt-basar    485 Mar 28 14:52  sedfile.txt
+-rw-rw-r--  1 rgukt-basar rgukt-basar     63 Mar 28 15:06  awkfile.txt
+-rw-------  1 rgukt-basar rgukt-basar  29577 Mar 28 15:21  .bash_history
+(base) rgukt-basar@rajesh-the-learner:~$ chmod  u+y numbers.txt
+chmod: invalid mode: ‘u+y’
+Try 'chmod --help' for more information.
+(base) rgukt-basar@rajesh-the-learner:~$ chmod  u+x numbers.txt
+(base) rgukt-basar@rajesh-the-learner:~$ man awk
+(base) rgukt-basar@rajesh-the-learner:~$ man chmod
+(base) rgukt-basar@rajesh-the-learner:~$ man sed
+(base) rgukt-basar@rajesh-the-learner:~$ man sort
+(base) rgukt-basar@rajesh-the-learner:~$ man resort
+No manual entry for resort
+(base) rgukt-basar@rajesh-the-learner:~$ man gopi
+No manual entry for gopi
+(base) rgukt-basar@rajesh-the-learner:~$ ping www. google .com
+ping: .com: Name or service not known
+(base) rgukt-basar@rajesh-the-learner:~$ 
+(base) rgukt-basar@rajesh-the-learner:~$ 
+(base) rgukt-basar@rajesh-the-learner:~$ ping www.google.com
+ping: www.google.com: Temporary failure in name resolution
+(base) rgukt-basar@rajesh-the-learner:~$ ping www.google.com
+PING www.google.com (142.250.66.4) 56(84) bytes of data.
+^Z
+[1]+  Stopped                 ping www.google.com
+(base) rgukt-basar@rajesh-the-learner:~$ ftp ftp.dlptest.com
+^Z
+[2]+  Stopped                 ftp ftp.dlptest.com
+(base) rgukt-basar@rajesh-the-learner:~$ ssh rajeshboda30@tty.sdf.org
+^Z                                                             
+[3]+  Stopped                 ssh rajeshboda30@tty.sdf.org
+(base) rgukt-basar@rajesh-the-learner:~$ ssh rajeshboda30@tty.sdf.org
+                                                 
+
+
+
